6c3cb1afca3b6255eb86610ddee8d8165f0728b4
qwon135 <82475863+qwon135@users.noreply.github.com>
2021-10-28 17:00:38 +0900

    21.10.28

---
+++ PythonDjango/add_two_numbers.js
@@ -0,0 +1,7 @@
+function doSomething(){
+    let a = document.getElementById('inputA').value;
+    let a = document.getElementById('inputB').value;
+    document.getElementById("valueA").innerHTML=a;
+    document.getElementById("valueB").innerHTML=b;
+    document.getElementById("valueC").innerHTML=Number(a)+Number(b);
+}
